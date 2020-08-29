957e09fe63a4091e3fdc1028bf264bde3281c95d
unknown <himanshugharat44@gmail.com>
2020-08-29 06:24:49 -0500

    ADDED: the code to get the invoice for single cab ride and minimum fare

---
+++ src/main/java/com/bridglabz/InvoiceGenerator.java
@@ -0,0 +1,16 @@
+package com.bridglabz;
+
+public class InvoiceGenerator {
+    private static final double COST_PER_KM = 10;
+    private static final int TIME_PER_MIN = 1;
+    private static final double MINIMUM_FARE = 5;
+    public double distance;
+    public int time;
+
+    public double calculateFare(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+        double totalFare = distance * COST_PER_KM + time * TIME_PER_MIN;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+}
---
+++ src/test/java/com/bridglabz/CabInvoiceTest.java
@@ -0,0 +1,30 @@
+package com.bridglabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CabInvoiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setup() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, totalFare, 0.0);
+    }
+}
